739b281932acc4638e6d9653f770e9bafdfdc0a6
Giuliano Scaglioni <scaglioni96@gmail.com>
2025-05-08 13:36:09 -0300

    fix en dbconfig

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({path:'variables.env'});
 
-module.exports = new Sequelize('torneo',process.env.DB_USER,process.env.DB_PASSWORD,{
+module.exports = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
